23ab94a0a348601d7b9b8453475a4b5576edcc77
Joseph Yoon <richrice@gmail.com>
2019-03-27 11:41:21 +0900

    first git test using eclipse

---
+++ git_eclipse/src/git_eclipse/git_eclipse.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package git_eclipse;
+
+/**
+ * @author user
+ *
+ */
+public class git_eclipse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
